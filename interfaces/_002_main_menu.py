8c36117c57b53ad1c6cee63ff02512681d0569ee
Special70 <josef.urquico@gmail.com>
2024-02-16 22:56:28 +0800

    Updates and Fixes
    - Reworked the method for checking keyhits whether to type on the search bar or go up/down on the side scroll bar

--- interfaces/_002_main_menu.py
+++ interfaces/_002_main_menu.py
@@ -8,16 +8,21 @@ from interfaces.func import arrow_scroll
 def _self():
     print(lang_obj._002_main_menu.get_text())
     while True:
-        if get_key_hit() == "up" or get_key_hit() == "down":
-            arrow_scroll(lang_obj._002_main_menu, "selector_bar", get_key_hit())
-            system('cls')
-            print(lang_obj._002_main_menu.get_text())
-        elif lang_obj._002_main_menu.get_selector_bar_val(0) == '►' and get_key_hit() != "":
-            lang_obj._002_main_menu.modify_search_text(get_key_hit())
-            system('cls')
-            #dbugprint("Added inputs to search bar")
-            reset_key_hit_val()
-            print(lang_obj._002_main_menu.get_text())
+        if get_key_hit():
+            match(get_key_hit()):
+                case "up" | "down":
+                    arrow_scroll(lang_obj._002_main_menu, "selector_bar", get_key_hit())
+                    system('cls')
+                    dbugprint("Arrow Scroll Occured")
+                    print(lang_obj._002_main_menu.get_text())
+                case _:
+                    if lang_obj._002_main_menu.get_selector_bar_val(0) == '►':
+                        if len(get_key_hit()) == 1 or get_key_hit() == "backspace":
+                            lang_obj._002_main_menu.modify_search_text(get_key_hit())
+                            system('cls')
+                            #dbugprint("Added inputs to search bar")
+                            reset_key_hit_val()
+                            print(lang_obj._002_main_menu.get_text())
         if get_key_hit() == "enter":
             if lang_obj._002_main_menu.get_selector_bar_val(1) == '►':
                 change_current_display("Front Menu")
